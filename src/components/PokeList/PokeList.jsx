c585ce3fe1abc62870ff9c2747e6663e9a87e577
shivam gupta <guptashivam3935@gmail.com>
2024-07-22 12:07:02 +0530

    adding next and prev url btn

--- src/components/PokeList/PokeList.jsx
+++ src/components/PokeList/PokeList.jsx
@@ -1,62 +1,90 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import Pokemon from '../Pokemon/Pokemon'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Pokemon from "../Pokemon/Pokemon";
 
 function Pokelist() {
-  
-    const [pokeList, setPokeList] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+  const [pokeList, setPokeList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const [pokedexUrl, setPokedexUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
 
-    async function downloadPokemon() {
-          const response = await axios.get("https://pokeapi.co/api/v2/pokemon")
-        //   console.log(response.data);
-            const promiseResult  = response.data.results
-            // console.log(promiseResult);
-            const pokemonPromiseResult = promiseResult.map((pokemon) => axios.get(pokemon.url) )
+  const [prevUrl, setPrevUrl] = useState("");
+  const [nextUrl, setNextUrl] = useState("");
 
-            const promiseData = await axios.all(pokemonPromiseResult)
-            
-            // console.log(pokemonPromiseResult);
-            console.log(promiseData);
+  async function downloadPokemon() {
+    setIsLoading(true);
+    const response = await axios.get(pokedexUrl);
+    //   console.log(response.data);
+    const promiseResult = response.data.results;
+    console.log(promiseResult);
+    const pokemonPromiseResult = promiseResult.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
 
-            const res = promiseData.map((pokeData) => {
-                const pokemon = pokeData.data
-                return{
-                    id:pokemon.id,
-                    name: pokemon.name,
-                    image : (pokemon.sprites.other)? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
-                    types: pokemon.types
-                }
-            })
+    const promiseData = await axios.all(pokemonPromiseResult);
 
-        console.log(res);
-        setPokeList(res)
-        setIsLoading(false)
-    }
+    // console.log(pokemonPromiseResult);
+    // console.log(promiseData);
 
-    useEffect(() => {
-        downloadPokemon()
-    }, [])
+    setPrevUrl(response.data.previous);
+    setNextUrl(response.data.next);
 
- 
+    const res = promiseData.map((pokeData) => {
+      const pokemon = pokeData.data;
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other
+          ? pokemon.sprites.other.dream_world.front_default
+          : pokemon.sprites.front_shiny,
+        types: pokemon.types,
+      };
+    });
+
+    console.log(res);
+    setPokeList(res);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    downloadPokemon();
+  }, [pokedexUrl]);
 
   return (
-    <div className='flex justify-around flex-col'>
-        <div className=' mx-auto text-3xl font-bold my-3 p-2'>
-            Pokemon List
-        </div>
-        <div className='flex flex-wrap justify-center items-center'>
-        {isLoading ?"loading": 
-         pokeList.map((e) => <Pokemon name={e.name} image={e.image} key={e.id}  />)
-        }
-        </div>
-        <div className='mt-3 flex justify-center items-center'>
-            <button className='px-3 py-1 mx-1 my-1.5 border border-slate-500	  hover:bg-slate-300 '> prev</button>
-            <button className='px-3 py-1 mx-1 my-1.5 border border-slate-500	  hover:bg-slate-300'>next</button>
-         </div>
+    <div className="flex justify-around flex-col">
+      <div className=" mx-auto text-3xl font-bold my-3 p-2">Pokemon List</div>
+      <div className="flex flex-wrap justify-center items-center ">
+        {isLoading
+          ? <div className="h-96 w-screen text-center text-5xl m-auto">loading</div>
+          : pokeList.map((e) => (
+              <Pokemon  name={e.name} image={e.image} key={e.id} />
+            ))}
+      </div>
+      <div className="mt-3 flex justify-center items-center">
+        <button
+          className="px-3 py-1 mx-1 my-1.5 border border-slate-300"
+          disabled={prevUrl==null}
+          onClick={() => {
+            setPokedexUrl(prevUrl);
+          }}
+        >
+          {" "}
+          prev
+        </button>
+        <button
+          className="px-3 py-1 mx-1 my-1.5 border border-slate-300"
+          disabled={nextUrl === null}
+          onClick={() => {
+            setPokedexUrl(nextUrl);
+          }}
+        >
+          next
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Pokelist
+export default Pokelist;
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Pokemon({name, image}) {
   return (
-    <div className='flex  justify-evenly items-center  flex-col '>
+    <div className='flex  justify-evenly items-center  flex-col border border-zinc-200 rounded-xl hover:bg-slate-200'>
           <div className='p-0'>
               {name}
           </div>
-         <div className='hover:bg-orange-100 p-6 mx-4  '> 
+         <div className=' p-4 mx-4  '> 
               <img className='max-w-50 max-h-40 ' src={image} alt="image" />
          </div>
       
